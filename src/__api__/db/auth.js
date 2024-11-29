8f64ad343ccb93dc018a4cab8b5495e51951d054
unknown <jlemes796@gmail.com>
2024-11-29 20:49:27 -0300

    removendo login fundation

--- src/__api__/db/auth.js
+++ src/__api__/db/auth.js
@@ -1,46 +1,27 @@
 import axios from "axios";
 
-const API_URL = "https://disparador-sqs.poli.digital/get-me-foundation";
+const API_URL = "https://omniapi-beta-polidigital.svc-us3.zcloud.ws/v3/auth";
 
 const authService = {
   login: async (email, password) => {
     try {
       const instance = axios.create({
-        withCredentials: true,
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
+        withCredentials: true, // Envia cookies automaticamente
       });
 
-      const params = new URLSearchParams();
-      params.append("email", email);
-      params.append("password", password);
-
-      const loginResponse = await instance.post(`${API_URL}`, params);
+      const loginResponse = await instance.post(`${API_URL}/login`, {
+        email,
+        password,
+      });
 
       if (loginResponse.status === 200 || loginResponse.status === 204) {
-        const userData = loginResponse.data;
-
-        if (userData.status === "ONLINE" && userData.roles.some(role => role.name === "adm")) {
-          // Armazena a resposta completa no localStorage
-          localStorage.setItem("user_data", JSON.stringify(userData));
-          console.log("Login bem-sucedido.");
-          return { authorized: true, message: "Usuário autorizado como admin." };
-        } else {
-          // Se o papel não for 'adm'
-          console.error("Você não é um admin.");
-          return { authorized: false, message: "Você não é um admin." };
-        }
+        console.log("Login bem-sucedido.");
+        return { authorized: true }; // Apenas indica sucesso
       } else {
         throw new Error("Credenciais inválidas.");
       }
     } catch (error) {
       console.error("Erro no serviço de autenticação:", error);
-
-      if (error.response?.data?.status === "Unauthenticated") {
-        return { authorized: false, message: "Usuário não autenticado." };
-      }
-
       throw new Error(error.response?.data?.message || "Erro ao autenticar.");
     }
   },
@@ -54,7 +35,6 @@ const authService = {
       const logoutResponse = await instance.post(`${API_URL}/logout`);
       if (logoutResponse.status === 200 || logoutResponse.status === 204) {
         console.log("Logout bem-sucedido.");
-        localStorage.removeItem("user_data");
       } else {
         throw new Error("Erro ao deslogar.");
       }
